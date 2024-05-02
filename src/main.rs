53a5814ac287fa9d79df31d7e6859a3e2f7111a8
Daya Adianto <addianto@users.noreply.github.com>
2024-05-02 12:28:26 +0700

    Implement async version based on rust-lang/async book

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,43 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{TcpListener, TcpStream},
-    time::Duration,
-};
+use std::{fs, time::Duration};
 
+use async_std::io::WriteExt;
+use async_std::net::TcpStream;
 use async_std::task;
+use async_std::{io::ReadExt, net::TcpListener};
+use futures::stream::StreamExt;
 
 #[async_std::main]
 async fn main() {
     // Recall previous experience in Computer Networks course,
     // especially about socket programming and binding to a port.
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream).await;
-    }
+    listener
+        .incoming()
+        .for_each_concurrent(/* limit */ None, |tcpstream| async move {
+            let stream = tcpstream.unwrap();
+            handle_connection(stream).await;
+        })
+        .await;
 
     println!("Shutting down.");
 }
 
 async fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    // TODO: Is it possible to use BufReader instead of manually-allocated buffer?
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).await.unwrap();
 
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            task::sleep(Duration::from_secs(5)).await;
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /SLEEP HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        task::sleep(Duration::from_secs(5)).await;
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
@@ -39,8 +45,6 @@ async fn handle_connection(mut stream: TcpStream) {
 
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    match stream.write_all(response.as_bytes()) {
-        Ok(_) => {}
-        Err(e) => eprintln!("Error writing to stream: {}", e),
-    }
+    stream.write(response.as_bytes()).await.unwrap();
+    stream.flush();
 }
